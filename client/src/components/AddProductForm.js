fdac4661669feb477c471d4a99240a820ea05bd8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-02-07 14:32:30 -0500

    Add toggle visible add product form

--- client/src/components/AddProductForm.js
+++ client/src/components/AddProductForm.js
@@ -1,37 +1,43 @@
-const AddProductForm = () => (
-  <div className="add-form">
-    <p>
-      <a role="button" className="button add-product-button">
-        Add A Product
-      </a>
-    </p>
-    <h3>Add Product</h3>
-    <form>
-      <div className="input-group">
-        <label htmlFor="product-name">Product Name</label>
-        <input type="text" id="product-name" value="" />
-      </div>
+import { useState } from 'react'
 
-      <div className="input-group">
-        <label htmlFor="product-price">Price</label>
-        <input type="text" id="product-price" value="" />
-      </div>
+const AddProductForm = () => {
+  const [formIsVisible, setFormIsVisible] = useState(false)
 
-      <div className="input-group">
-        <label htmlFor="product-quantity">Quantity</label>
-        <input type="text" id="product-quantity" value="" />
-      </div>
-
-      <div className="actions form-actions">
-        <a role="button" className="button">
-          Add
+  return (
+    <div className={formIsVisible ? 'add-form visible' : 'add-form'}>
+      <p>
+        <a role="button" className="button add-product-button" onClick={() => setFormIsVisible(true)}>
+          Add A Product
         </a>
-        <a role="button" className="button">
-          Cancel
-        </a>
-      </div>
-    </form>
-  </div>
-)
+      </p>
+      <h3>Add Product</h3>
+      <form>
+        <div className="input-group">
+          <label htmlFor="product-name">Product Name</label>
+          <input type="text" id="product-name" value="" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="product-price">Price</label>
+          <input type="text" id="product-price" value="" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="product-quantity">Quantity</label>
+          <input type="text" id="product-quantity" value="" />
+        </div>
+
+        <div className="actions form-actions">
+          <a role="button" className="button">
+            Add
+          </a>
+          <a role="button" className="button" onClick={() => setFormIsVisible(false)}>
+            Cancel
+          </a>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default AddProductForm

fd921b4a29245cb5f61909e29e1b1970e53d070f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-02-09 14:24:54 -0500

    test EditProductFrom submits with updated inputs

--- client/src/components/EditProductForm.test.js
+++ client/src/components/EditProductForm.test.js
@@ -3,19 +3,19 @@ import userEvent from '@testing-library/user-event'
 
 import EditProductForm from './EditProductForm'
 
-const props = {
-  id: '6202bcc6cee6f53cf35dcc44',
-  title: 'Amazon Kindle E-reader',
-  quantity: 20,
-  price: 79.99,
-  onEditProduct: jest.fn(),
-  onCancelEdit: jest.fn(),
-}
-
 describe('EditProductForm', () => {
-  let nameInput, priceInput, quantityInput
+  let props, nameInput, priceInput, quantityInput
 
   beforeEach(() => {
+    props = {
+      id: '6202bcc6cee6f53cf35dcc44',
+      title: 'Amazon Kindle E-reader',
+      quantity: 20,
+      price: 79.99,
+      onEditProduct: jest.fn(),
+      onCancelEdit: jest.fn(),
+    }
+
     render(<EditProductForm {...props} />)
     nameInput = screen.getByLabelText('Product Name')
     priceInput = screen.getByLabelText('Price')
@@ -36,6 +36,21 @@ describe('EditProductForm', () => {
     expect(props.onEditProduct.mock.calls.length).toBe(1)
     expect(props.onEditProduct.mock.calls[0][1]).toEqual({
       title: props.title,
+      price: props.price,
+      quantity: props.quantity,
+    })
+  })
+
+  test('Submitting form with updated fields submits the new values', () => {
+    const updateButton = screen.getByRole('button', { name: /update/i })
+    userEvent.clear(nameInput)
+    userEvent.type(nameInput, 'New Item')
+    userEvent.click(updateButton)
+
+    expect(props.onEditProduct.mock.calls[0][0]).toBe(props.id)
+    expect(props.onEditProduct.mock.calls.length).toBe(1)
+    expect(props.onEditProduct.mock.calls[0][1]).toEqual({
+      title: 'New Item',
       price: props.price,
       quantity: props.quantity,
     })

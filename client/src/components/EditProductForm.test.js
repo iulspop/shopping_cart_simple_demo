c2f5d4555ffe92c1e66843544837db027201983f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-02-09 14:17:57 -0500

    test EditProductForm calls onEditProducto on submit

--- client/src/components/EditProductForm.test.js
+++ client/src/components/EditProductForm.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import EditProductForm from './EditProductForm'
 
@@ -12,15 +13,31 @@ const props = {
 }
 
 describe('EditProductForm', () => {
-  test('Form is populated with product info', async () => {
+  let nameInput, priceInput, quantityInput
+
+  beforeEach(() => {
     render(<EditProductForm {...props} />)
+    nameInput = screen.getByLabelText('Product Name')
+    priceInput = screen.getByLabelText('Price')
+    quantityInput = screen.getByLabelText('Quantity')
+  })
 
-    const nameInput = screen.getByLabelText('Product Name')
-    const priceInput = screen.getByLabelText('Price')
-    const quantityInput = screen.getByLabelText('Quantity')
-
+  test('Form is populated with product info', () => {
     expect(nameInput.value).toBe(props.title)
     expect(priceInput.value).toBe(props.price.toString())
     expect(quantityInput.value).toBe(props.quantity.toString())
+  })
+
+  test('Submitting form calls onEditProduct with correct values', () => {
+    const updateButton = screen.getByRole('button', { name: /update/i })
+    userEvent.click(updateButton)
+
+    expect(props.onEditProduct.mock.calls[0][0]).toBe(props.id)
+    expect(props.onEditProduct.mock.calls.length).toBe(1)
+    expect(props.onEditProduct.mock.calls[0][1]).toEqual({
+      title: props.title,
+      price: props.price,
+      quantity: props.quantity,
+    })
   })
 })

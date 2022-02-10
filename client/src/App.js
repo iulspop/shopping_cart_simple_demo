e78960f9e9060b3769bf6300c68f00a496b575bf
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-02-10 14:52:04 -0500

    Remove unused comments

--- client/src/App.js
+++ client/src/App.js
@@ -37,33 +37,3 @@ const App = () => {
 }
 
 export default App
-
-/*
-x Create a store
-x Create a provider
-x Create a root reducer
-- In each component we select the properties we need using useSelector
-- In each component where actions are created, dispatch those actions
-- Write action creators
-- Factor out individual reducers
-*/
-
-/*
-Product
-- does it load product list X
-  READ
-- submit add product form, adds a product X
-  CREATE
-- submit edit form, edits product X
-  UPDATE
-- click X button on product, deletes product X
-  DELETE
-
-Cart
-- does it load cart items X
-- click add to cart, adds cart item X
-  - updates quantity in product list and cart
-  - when product quantity is 0, disable button and turn text red
-- click checkout, clear cart
-
-*/
